5961d87bc71aa56dff3abb20ad8fdb5e35c04dae
Your <kacper.ferber@outlook.com`>
2025-10-10 22:45:10 +0200

    trafka

--- scripts/map.gd
+++ scripts/map.gd
@@ -16,18 +16,16 @@ func world_to_tile(pos:Vector2)->Vector2i:
 	
 func generate_map():
 	var chunk = Vector2i(0, 0)
-	var source_id = 0
-	var atlas_coords = Vector2i(1, 5)
+	var source_id = 2
+	var atlas_coords = Vector2i(0, 0)
 	while true:
 		chunk = get_chunk(chunk)
-		var position = world_to_tile(-1*(player.global_position))
-		for x in range(position.x-size/2, position.x+size/2):
-			for y in range(position.y-size/2, position.y+size/2):
+		var pozycja = world_to_tile(-1*(player.global_position))
+		for x in range(pozycja.x-size/2, pozycja.x+size/2):
+			for y in range(pozycja.y-size/2, pozycja.y+size/2):
 				tile_layer.set_cell(Vector2i(x,y), source_id, atlas_coords)
 		await get_tree().create_timer(0.1).timeout		
-
-
-
+		
 func get_chunk(chunk: Vector2i)->Vector2i:
 	chunk = world_to_tile(-1*(player.global_position))/size
 	print(world_to_tile(player.global_position))
