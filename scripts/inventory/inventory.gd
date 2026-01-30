c63447a77194d611f25825fec7e5d5b2b6e1cd18
ArtiChaos <80818691+ArtiFact-Code@users.noreply.github.com>
2026-01-30 18:26:18 +0100

    'working' inventory system

--- scripts/inventory/inventory.gd
+++ scripts/inventory/inventory.gd
@@ -1,8 +1,16 @@
-extends Node2D
+extends Control
 
 var is_open: bool = false
 var inventory_data: Dictionary = {}
 var player: Node = null
+
+@onready var grid := $body/BackpackGrid
+var invSlot := preload("res://scenes/inv_slot.tscn")
+
+var all_slots: Array[Control] = []
+
+var current_page := 0
+var items_per_page := 10
 
 func _ready() -> void:
 	await get_tree().process_frame  
@@ -25,8 +33,8 @@ func toggle_inventory() -> void:
 	
 	if is_open:
 		print("Inventory opened")
-		show_inventory()
 		update_display()
+		show_inventory()
 	else:
 		print("Inventory closed")
 		hide_inventory()
@@ -37,14 +45,26 @@ func show_inventory() -> void:
 func hide_inventory() -> void:
 	visible = false
 
-func update_display() -> void:
+func show_page(page: int) -> void:
+	var start := page*items_per_page
+	var end = start + items_per_page
 	
+	for child in grid.get_children():
+		grid.remove_child(child)
+		
+	for i in range(start, min(end, all_slots.size() - start)):
+		grid.add_child(all_slots[i])
+	
+func update_display() -> void:	
+	show_page(0)
 	print("Current inventory:", inventory_data)
 
 func _on_mineral_collected(mineral_type: String, quality: float) -> void:
 	if not inventory_data.has(mineral_type):
 		inventory_data[mineral_type] = []
-	
+		
+	all_slots.append(invSlot.instantiate())
+		
 	inventory_data[mineral_type].append(quality)
 	print("Inventory updated: ", mineral_type, " quality: ", quality)
 	
