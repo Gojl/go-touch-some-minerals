ecf9fa39973f7e8ccb527bc560e272750f0531b4
ArtiChaos <80818691+ArtiFact-Code@users.noreply.github.com>
2026-02-03 18:49:21 +0100

    quick cursor icon hover fix

--- scripts/inventory/button_prev.gd
+++ scripts/inventory/button_prev.gd
@@ -4,3 +4,12 @@ func _pressed() -> void:
 	var inv = get_tree().get_first_node_in_group("inventory")
 	if inv:
 		inv.prev_page()
+
+func _mouse_entered() -> void:
+	CursorManager.set_cursor("select")
+func _mouse_exited() -> void:
+	CursorManager.reset_cursor()
+
+func _ready() -> void:
+	mouse_entered.connect(_mouse_entered)
+	mouse_exited.connect(_mouse_exited)
